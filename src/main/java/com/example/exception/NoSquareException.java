529065dc94ef5e227542192a558cbfc099b7093f
Mounir <lm_melzi@esi.dz>
2025-01-08 01:18:28 +0100

    upload code

---
+++ src/main/java/com/example/exception/NoSquareException.java
@@ -0,0 +1,14 @@
+package com.example.exception;
+
+public class NoSquareException extends Exception {
+	public NoSquareException() {
+		super();
+		// TODO Auto-generated constructor stub
+	
+	}
+
+	public NoSquareException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+}
